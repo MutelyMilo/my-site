9bf7c680102a5e40f6e7ff9d6499086df337d28a
Phoenix <phoenix@Phoenixs-MacBook-Pro.local>
2023-08-21 22:44:50 +0800

    feat: add vercel/analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Script from "next/script";
 import Head from "next/head";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +22,10 @@ export default function RootLayout({
         <Head>
             <script src="//res.wx.qq.com/open/js/jweixin-1.2.0.js" async></script>
         </Head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      {children}
+      <Analytics />
+      </body>
     </html>
   )
 }
