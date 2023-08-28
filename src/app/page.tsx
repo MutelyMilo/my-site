1d50fbba705c13740f47181565879fcff7627323
poplar <mutelymilo@gmail.com>
2023-08-28 20:01:10 +0800

    chore: update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import styles from './page.module.scss';
+import MenuButton from '@/components/pc/MenuButton';
 
 const Page = () => {
 	return (
 		<div className={styles.homeContainer}>
+			<MenuButton />
 			<p className={styles.homeContainerTitle}>Poplar&apos;s Blog</p>
 		</div>
 	);
---
+++ src/components/pc/MenuButton/index.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import styles from './style.module.scss';
+import { useCallback, useState } from 'react';
+
+const MenuButton = () => {
+	const [open, setOpen] = useState(false);
+	const handleMenuOpen = useCallback(() => {
+		setOpen(!open);
+	}, [open, setOpen]);
+	return (
+		<div
+			onClick={handleMenuOpen}
+			className={`${styles.menuButtonContainer} ${open && styles.act}`}
+		>
+			<div className={styles.menuButton}></div>
+		</div>
+	);
+};
+
+export default MenuButton;
